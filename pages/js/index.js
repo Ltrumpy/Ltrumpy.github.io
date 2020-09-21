93f9019987745460289ad98e46fc6d6fe3340cd3
Ltrumpy <Lindsay.Trump@gmail.com>
2020-09-20 21:40:35 -0700

    JS loader added

---
+++ pages/js/index.js
@@ -0,0 +1,7 @@
+setTimeout(function () {
+    document.getElementById("loading-page").className += "loaded";
+    document.getElementById("loader").className += "opzero";
+    document.getElementById("lastray").className += " finalray";
+    document.body.className += "whitebk";
+  }, 6500);
+  
